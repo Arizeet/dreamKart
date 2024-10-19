8d06e44f52cfea2e23b3b591dca0aebb5b312244
Arizeet Behera <92688073+Arizeet@users.noreply.github.com>
2024-10-19 20:30:00 +0530

    Initial commit

---
+++ src/test/java/com/arizeet/dreamKart/DreamKartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arizeet.dreamKart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamKartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
